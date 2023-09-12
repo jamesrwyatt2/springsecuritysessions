57bdc2814d976bb68d987099991c69bf577bde87
James Wyatt <jamesrwyatt35@gmail.com>
2023-09-11 21:13:50 -0400

    commit

--- src/test/java/com/example/springsecuritysessions/SpringsecuritysessionsApplicationTests.java
+++ src/test/java/com/example/springsecuritysessions/SpringsecuritysessionsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringsecuritysessionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
