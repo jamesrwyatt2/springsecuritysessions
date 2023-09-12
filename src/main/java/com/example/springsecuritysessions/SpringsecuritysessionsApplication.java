c55215a3bf92e462727810ec8b1a4b6db1bcd358
James Wyatt <jamesrwyatt35@gmail.com>
2023-09-11 21:13:42 -0400

    init commit

---
+++ src/main/java/com/example/springsecuritysessions/SpringsecuritysessionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritysessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritysessionsApplication.class, args);
+	}
+
+}
